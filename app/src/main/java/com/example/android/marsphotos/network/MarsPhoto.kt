85b3762b76efdef83d215006c55354f9eebb7ccc
Morfz <100391684+Morfz@users.noreply.github.com>
2022-05-22 19:33:53 +0800

    initial commit

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -22,6 +22,7 @@ import com.squareup.moshi.Json
  * This data class defines a Mars photo which includes an ID, and the image URL.
  * The property names of this data class are used by Moshi to match the names of values in JSON.
  */
+
 data class MarsPhoto(
         val id: String,
         // used to map img_src from the JSON to imgSrcUrl in our class
